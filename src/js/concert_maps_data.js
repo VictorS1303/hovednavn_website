9fdf33517a66767cd3f7e6c697ab475d7fc86819
Victor Schweitz <victorschweitz@hotmail.com>
2025-05-10 12:12:23 +0200

    Concert card maps finished

---
+++ src/js/concert_maps_data.js
@@ -0,0 +1,42 @@
+export const concertCardsData = [
+    {
+      concertDate: '5. maj',
+      concertLocation: 'Kayak Bar',
+      concertImage: 'concert_card_bg',
+      concertPrice: '100',
+      concertLocationCoordinates: {
+        lat: 55.6755814661335,
+        lng: 12.586671251447852,
+      },
+    },
+    {
+      concertDate: '23. juni',
+      concertLocation: 'Pumpehuset',
+      concertImage: 'concert_card_bg',
+      concertPrice: '100',
+      concertLocationCoordinates: {
+        lat: 55.67739435279951,
+        lng: 12.565182724463128,
+      },
+    },
+    {
+      concertDate: '26. juli',
+      concertLocation: 'Loppen',
+      concertImage: 'concert_card_bg',
+      concertPrice: '130',
+      concertLocationCoordinates: {
+        lat: 55.67389982358226,
+        lng: 12.597148928165135,
+      },
+    },
+    {
+      concertDate: '30. august',
+      concertLocation: 'Amager Bio',
+      concertImage: 'concert_card_bg',
+      concertPrice: '130',
+      concertLocationCoordinates: {
+        lat: 55.65415790187533,
+        lng: 12.609199896636788,
+      },
+    },
+]
